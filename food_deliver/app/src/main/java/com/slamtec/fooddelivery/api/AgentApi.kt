563f20919402182d97b1c7298c0e258839e5f89d
qikun.xiong <qikun.xiong@slamtec.com>
2021-10-21 10:13:13 +0800

    update agent api

--- food_deliver/app/src/main/java/com/slamtec/fooddelivery/api/AgentApi.kt
+++ food_deliver/app/src/main/java/com/slamtec/fooddelivery/api/AgentApi.kt
@@ -10,22 +10,22 @@ import retrofit2.http.*
  * Date   : 2021/8/6 6:28 PM
  */
 interface AgentApi {
-    @GET("core/systemresource/v1/powerstatus")
+    @GET("core/system/v1/power/status")
     fun queryPowerStatus(): Call<PowerStatus>
 
-    @GET("core/systemresource/v1/robothealth")
+    @GET("core/system/v1/robot/health")
     fun queryRobotHealth(): Call<RobotHealth>
 
     @GET("multi-floor/map/v1/pois")
     fun queryPois(): Call<ArrayList<Poi>>
 
-    @GET("delivery/v1/password")
+    @GET("delivery/v1/admin/password")
     fun queryPassword(): Call<PasswordResponse>
 
     @POST("core/motion/v1/actions")
     fun createMoveAction(@Body action: CreateMoveAction): Call<ActionResponse>
 
-    @POST("core/systemresource/v1/shutdown")
+    @POST("core/system/v1/power/:shutdown")
     fun shutdown(@Body shutdownRequest: ShutdownRequest): Call<Boolean>
 
     @GET("core/motion/v1/actions/{action_id}")
@@ -34,12 +34,12 @@ interface AgentApi {
     @DELETE("core/motion/v1/actions/:current")
     fun stopCurrentAction(): Call<Void>
 
-    @GET("core/systemresource/v1/deviceinfo")
+    @GET("core/system/v1/robot/info")
     fun getDeviceInfo(): Call<DeviceInfo>
 
-    @GET("core/systemresource/v1/systemparameter")
+    @GET("core/system/v1/parameter")
     fun getSystemParameter(@Query("param") param: String): Call<String>
 
-    @PUT("core/systemresource/v1/systemparameter")
+    @PUT("core/system/v1/parameter")
     fun setSystemParameter(@Body systemParameter: SystemParameter): Call<Boolean>
 }
